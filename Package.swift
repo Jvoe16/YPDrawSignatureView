d5c727fa12a5f63bab72da68328279cb8b8b3e6a
Jvoe16 <56768946+Jvoe16@users.noreply.github.com>
2020-02-18 10:42:28 -0700

    Update Package.swift
    Swift Package Manager Support Xcode11

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "YPDrawSignature"
+    name: "YPDrawSignature",
+    platforms: [.iOS(.v8), .tvOS(.v9)],
+    products: [
+        .library(name: "YPDrawSignature", targets: ["YPDrawSignature"])
+    ],
+    targets: [
+        .target(name: "YPDrawSignature")
+    ]
 )
